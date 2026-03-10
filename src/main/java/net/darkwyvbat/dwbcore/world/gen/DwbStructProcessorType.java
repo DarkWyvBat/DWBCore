b3c703600cee905b86d86ce23716ebf6e802380c
DarkWyvBat <darkwyvbatp@gmail.com>
2026-03-11 00:26:11 +0700

    Advancement translates

---
+++ src/main/java/net/darkwyvbat/dwbcore/world/gen/DwbStructProcessorType.java
@@ -0,0 +1,14 @@
+package net.darkwyvbat.dwbcore.world.gen;
+
+import net.darkwyvbat.dwbcore.registry.RegistrationHelper;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
+
+import static net.darkwyvbat.dwbcore.DwbCore.INFO;
+
+public class DwbStructureProcessorType {
+
+    public static final StructureProcessorType<ProxyBlockProcessor> PROXY_BLOCK = RegistrationHelper.registerStructProcessor(INFO.id("proxy_block"), ProxyBlockProcessor.CODEC);
+
+    public static void init() {
+    }
+}
